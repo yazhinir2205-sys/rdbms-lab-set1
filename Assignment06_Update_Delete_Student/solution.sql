25816408cc2ed666241275d1895893fe259fe495
yazhinir2205-sys <yazhini.r2205@gmail.com>
2026-08-17 12:06:45 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -31,3 +31,24 @@ DELETE FROM Student
 WHERE StudentID = 1002;
 
 SELECT * FROM Student;
+USE CollegeDB;
+
+CREATE TABLE Marksheet (
+    RollNo INT(5) PRIMARY KEY,
+    Name VARCHAR(20),
+    Department VARCHAR(10),
+    Marks INT(3)
+);
+
+INSERT INTO Marksheet (RollNo, Name, Department, Marks)
+VALUES
+(1, 'Arun', 'CSE', 85),
+(2, 'Divya', 'IT', 78),
+(3, 'Karthik', 'CSE', 92),
+(4, 'Nisha', 'ECE', 67),
+(5, 'Rahul', 'IT', 88);
+
+SELECT *
+FROM Marksheet
+WHERE Marks > 80
+ORDER BY Marks DESC;
