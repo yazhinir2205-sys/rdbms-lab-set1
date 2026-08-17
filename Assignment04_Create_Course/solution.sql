131b61e27d93b9fcfa68d8e6dab095fbbdc22196
yazhinir2205-sys <yazhini.r2205@gmail.com>
2026-08-17 12:04:03 +0530

    Create Course table and insert sample records
    Added Course table creation and sample data insertion.

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -7,3 +7,23 @@ USE CollegeDB;
 -- Insert three records
 
 -- Display structure
+USE CollegeDB;
+
+CREATE TABLE Course (
+    CourseID INT(5) PRIMARY KEY,
+    CourseName VARCHAR(30) NOT NULL,
+    Credits INT(2) NOT NULL,
+    DepartmentID INT(5) NOT NULL
+);
+
+INSERT INTO Course (CourseID, CourseName, Credits, DepartmentID)
+VALUES
+(101, 'Python', 4, 1),
+(102, 'Web Technology', 3, 1),
+(103, 'Database Management', 4, 2);
+
+SELECT * FROM Course;
+
+DESCRIBE Department;
+DESCRIBE Student;
+DESCRIBE Course;
